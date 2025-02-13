95b740ef582c77519545d9f1c6caed73d42ac632
IsaacWiscox <Isaacw@wiscox.com>
2025-02-13 16:55:39 -0500

    Finished HTML and .js
    Cleaned up HTML and finished app.js
    
    Footer now appends current year

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -1,2 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("copyright").append(new Date().getFullYear());
+})
 
-document.getElementById("copyright").textContent += " -asd " + new Date().toLocaleDateString();
+
+
+// let counter = 0;
+
+
+
+// document.addEventListener("click", function () {
+//     counter = counter + 1;
+//     console.log(counter)
+//     if (counter == 5) {
+//         console.log("Clicked 5 times");
+//     };
+
+// });
